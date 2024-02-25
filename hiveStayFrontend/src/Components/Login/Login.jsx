8e47f980d3e0da3a334a6f3792012e6dacca5bf4
tanishkh16 <tanishkhandelwaldsa16@gmail.com>
2024-02-25 11:34:40 +0530

    changes done

--- hiveStayFrontend/src/Components/Login/Login.jsx
+++ hiveStayFrontend/src/Components/Login/Login.jsx
@@ -3,57 +3,6 @@ import {  useNavigate ,Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { signInStart,signInFailure,signInSuccess } from "../../../redux/user/userSlice.js";
 
-export default function Login() {
-  const dispatch=useDispatch();
-  const history = useNavigate();
-  const [formdata,setFormdata]=useState({});
-  const { error, loading } = useSelector((state) => state.user);
-  
-  const handleChange=(e)=>{
-    setFormdata({
-      ...formdata,
-      [e.target.id]:e.target.value
-    })
-    console.log(formdata);
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    dispatch(signInStart());
-
-    const config = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        credentials: 'include',
-        body: JSON.stringify(formdata),
-    };
-
-    try {
-        const response = await fetch('http://localhost:3000/api/login', config);
-       
-        const data = await response.json();
-        if (data.success === false) {
-            dispatch(signInFailure(data.message));
-            history("/login");
-            return;
-        }
-        dispatch(signInSuccess(data));
-        history("/");
-    } catch (error) {
-        dispatch(signInFailure('An error occurred during login.'));
-        history("/login");
-    }
-}
-  return (
-    <div>
-      
-    </div>
-=======
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FaGoogle } from "react-icons/fa";
-
 
 export default function Login() {
   const backgroundImageStyle = {
